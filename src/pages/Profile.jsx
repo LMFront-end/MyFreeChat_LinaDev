bd33ef1ef339bbda270f14ba641689ddae5b1239
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-03 22:40:10 -0500

    se refactoriza el codigo, fix bugs con las rules de firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Camera } from '../components/svg/Camera'
+import { Delete } from '../components/svg/Delete'
 import { storage, db, auth } from '../../data/configFirebase'
 import { 
     ref, 
@@ -7,6 +8,7 @@ import {
     uploadBytes 
 } from 'firebase/storage';
 import {getDoc, doc, updateDoc} from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom';
 
 import Img from '../../assets/user.png';
 
@@ -14,74 +16,90 @@ const Profile = () => {
 
     const [img, setImg] = useState("");
     const [user, setUser] = useState();
-
+    const history = useNavigate("");
 
     useEffect(() => {
+        getDoc(doc(db, "users", auth.currentUser.uid)).then((docSnap) => {
+            if (docSnap.exists) {
+            setUser(docSnap.data());
+            }
+        });
 
-        getDoc(doc(db, 'users', auth.currentUser.uid))
-            .then(docSnap => {
-                if(docSnap.exists){
-                    setUser(docSnap.data());
-                }
+        if (img) {
+            const uploadImg = async () => {
+            const imgRef = ref(
+            storage,
+            `avatar/${new Date().getTime()} - ${img.name}`
+            );
+
+        try {
+            if (user.avatarPath) {
+            await deleteObject(ref(storage, user.avatarPath));
+        }
+            const snap = await uploadBytes(imgRef, img);
+            const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
+
+            await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: url,
+            avatarPath: snap.ref.fullPath,
+        });
+
+            setImg("");
+        } catch (err) {
+            console.log(err.message);
+            }
+        };
+        uploadImg();
+        }
+    }, [img]);
+
+    const deleteImage = async () => {
+        try {
+            const confirm = window.confirm("Delete avatar?");
+            if (confirm) {
+            await deleteObject(ref(storage, user.avatarPath));
+
+            await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            avatar: "",
+            avatarPath: "",
             });
 
-        if(img){
-            const uploadImg = async () => {
-                const imgRef = ref(
-                    storage, 
-                    `avatar/${new Date().getTime()} - ${img.name}`);
-
-                try {
-
-                const snap = await uploadBytes(imgRef, img);
-
-                const url = await getDownloadURL(ref(storage, snap.ref.fullPath));
-
-                await updateDoc(doc(db, 'users', auth.currentUser.uid), {
-                    avatar: url,
-                    avatarPath: snap.ref.fullPath,
-                });
-
-                setImg("");
-
-                }catch(err){
-                    console.log(err.message);
-                }
-
-            };
-
-            uploadImg();
+            history("/home");
+            }
+        } catch (err) {
+        console.log(err.message);
         }
-
-    }, [img])
-
-    return (
+    };
+  return user ? (
     <section>
-        <div className="profile_container">
-            <div className="img_container">
-                <img src={Img} alt="avatar" />
-                <div className="overlay">
-                    <label htmlFor="photo">
-                        <Camera />
-                    </label>
-                    <input 
-                    type="file" accept="image/*" 
-                    style={{display: "none"}} 
-                    id='photo' 
-                    onChange={(e) => setImg(e.target.files[0])}
-                    />
-                </div>
+      <div className="profile_container">
+        <div className="img_container">
+          <img src={user.avatar || Img} alt="avatar" />
+          <div className="overlay">
+            <div>
+              <label htmlFor="photo">
+                <Camera />
+              </label>
+              {user.avatar ? <Delete deleteImage={deleteImage} /> : null}
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                id="photo"
+                onChange={(e) => setImg(e.target.files[0])}
+              />
             </div>
-            <div className="text_container">
-                <h3>Name</h3>
-                <p>User email</p>
-                <hr />
-                <small>Joined on: ---</small>
-            </div>
+          </div>
         </div>
-
+        <div className="text_container">
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <hr />
+          <small>Joined on: {user.createdAt.toDate().toDateString()}</small>
+        </div>
+      </div>
     </section>
-    )
+  ) : null;
 }
 
 export {Profile}
