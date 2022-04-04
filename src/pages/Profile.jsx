bade2ff92806a21678b7b5c1f79b0acbc8199182
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-03 19:47:27 -0500

    se crea un input file para cambiar foto de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Camera } from '../components/svg/Camera'
 
 import Img from '../../assets/user.png';
 
@@ -8,6 +9,12 @@ const Profile = () => {
         <div className="profile_container">
             <div className="img_container">
                 <img src={Img} alt="avatar" />
+                <div className="overlay">
+                    <label htmlFor="photo">
+                        <Camera />
+                    </label>
+                    <input type="file" accept="image/*" style={{display: "none"}} id='photo' />
+                </div>
             </div>
             <div className="text_container">
                 <h3>Name</h3>
